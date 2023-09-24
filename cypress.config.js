ccaf221d64354a5ae0082fdbf2699edabef43742
Fernando Papito <fernandorcks@gmail.com>
2023-09-24 15:23:52 -0300

    vai planeta

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("cypress");
+const { configureAllureAdapterPlugins } = require('@mmisty/cypress-allure-adapter/plugins');
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      configureAllureAdapterPlugins(on, config);
+      return config;
+    },
+    env: {
+      allure: true,
+      allureAddVideoOnPass: true
+    },
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    baseUrl: "https://loginxp.vercel.app",
+    video: true
+  }
+});

c04fe3a1c89067fc9f4913090ef27e6106be7d8a
Fernando Papito <fernandorcks@gmail.com>
2023-09-25 06:44:21 -0300

    agora vai

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,30 +5,30 @@ describe('Login', () => {
     it ('usuário obrigatório', ()=> {
         cy.login(null, 'teste')
         cy.get(toast)
-            .should('have.text', 'Informe o seu nome de usuáriu!')
+            .should('have.text', 'Informe o seu nome de usuário!')
     })
 
     it ('senha obrigatória', ()=> {
         cy.login('qa', null)
         cy.get(toast)
-            .should('have.text', 'Informe a sua senha secret4!')
+            .should('have.text', 'Informe a sua senha secreta!')
     })
 
     it('usuário não existe', () => {
         cy.login('teste', 'teste')
         cy.get(toast)
-            .should('have.text', 'Oops! Credenciais inválidaz :(')
+            .should('have.text', 'Oops! Credenciais inválidas :(')
     })
 
     it('senha incorreta', () => {
         cy.login('qa', 'teste')
         cy.get(toast)
-            .should('have.text', 'Oops! Credenciais inválidaz :(')
+            .should('have.text', 'Oops! Credenciais inválidas :(')
     })
 
     it('com sucesso', () => {
         cy.login('qa', 'xperience')
         cy.get(modalContent)
-            .should('have.text', 'Suas credenciais são válidaz :)')
+            .should('have.text', 'Suas credenciais são válidas :)')
     })
 })
